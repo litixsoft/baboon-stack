308918b593dac6dddb7fc541e0f7631380214726
Thomas Scheibe <th.scheibe@litixsoft.de>
2013-07-29 11:07:41 +0200

    Initial commit. MacOS Skripte.

---
+++ macos/uninstall.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Install Script for BaboonStack
+echo "BaboonStack De-Installer"
+echo
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+
+function request {
+  read -p "$1: [Y/n] " answer
+  case "$answer" in
+    Yes|yes|Y|y|"") return 0 ;;
+    No|no|N|n) return 1 ;;
+        *) return 1 ;;
+  esac
+}
+
+# Some variables
+LXHOMEPATH="$(dirname $(readlink ${BASH_SOURCE[0]} || echo ${BASH_SOURCE[0]#*/}))"
+LXBINPATH="/usr/bin"
+LXMODE=""
+
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  echo
+  exit 1
+fi
+
+if ( request "Remove Databases, Configuration files, etc?" ) then
+  LXMODE="all"
+fi
+
+# Execute every lxscript.sh
+files=`find "$LXHOMEPATH" -maxdepth 2 -type f -name "lxscript.sh"`
+for scriptfile in $files; do
+  if [ -x "$scriptfile" ]; then
+    sh "$scriptfile" remove $LXMODE
+  fi
+done
+
+# Remove Node.JS
+if [ -d "$LXHOMEPATH/node" ]; then
+  # Remove Symlink
+  echo "Remove Node.JS"
+  rm "$LXBINPATH/node"
+  rm "$LXBINPATH/npm"
+  rm -rf "$LXHOMEPATH/node"
+fi
+
+# Remove lxManager
+if [ -d "$LXHOMEPATH/lxm" ]; then
+  # Remove Symlink
+  echo "Remove lxManager"
+  rm "$LXBINPATH/lxm"
+  rm -rf "$LXHOMEPATH/lxm"
+fi
+
+if [ "$LXMODE" = "all" ]; then
+  echo "Remove $LXHOMEPATH"
+  rm -rf "$LXHOMEPATH"
+fi
+
+echo "Done! Bye..."
+echo
