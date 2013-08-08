d28a11b62a9ea94796efe1611af34bcc2968cc7f
Thomas Scheibe <th.scheibe@litixsoft.de>
2013-08-08 16:12:38 +0200

    Kleines seperates Tool zur Erstellung der Checksum Datei (SHASUMS.txt).

---
+++ win/buildChecksum.py
@@ -0,0 +1,53 @@
+#-------------------------------------------------------------------------------
+# Name:        buildChecksum
+# Purpose:
+#
+# Author:      Thomas Scheibe
+#
+# Created:     08.08.2013
+# Copyright:   (c) Thomas Scheibe 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import lxtools
+import sys
+import os
+
+def header():
+    print('buildChecksum - SHA1 Checksum Generator\n')
+
+def help():
+    print('Usage:\n')
+    print('  buildchecksum [folder]\n')
+
+def main(folder):
+    fileList = os.listdir(folder)
+    shasumFilename = os.path.join(folder, 'SHASUMS.txt')
+    hashList = []
+
+    for entry in fileList:
+        fullFilename = os.path.join(folder, entry)
+
+        if entry.lower() != 'shasums.txt' and os.path.isfile(fullFilename):
+            print('Hash {0}'.format(entry))
+
+            result = []
+            result.append(lxtools.getSHAChecksum(fullFilename))
+            result.append(entry)
+
+
+            hashList.append(result)
+
+    # Build SHASUM.TXT
+    shasumFile = open(shasumFilename, 'w')
+    for entry in hashList:
+        shasumFile.write('  '.join(entry) + '\n')
+    shasumFile.close()
+    pass
+
+if __name__ == '__main__':
+    header()
+
+    if len(sys.argv) < 2 or not os.path.isdir(sys.argv[1]):
+        help()
+    else:
+        main(sys.argv[1])
