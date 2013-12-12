90a9778194e0c58818c24ceb5654f388e9740c0b
Thomas Scheibe <th.scheibe@litixsoft.de>
2013-12-12 16:30:38 +0100

    Build script

---
+++ make.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 build.py bbs.py --target-dir "build" --icon "ressources/baboonstack.ico" --compress
--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup( name = "bbs",
-       version = "1.3.0",
-       description = "Baboonstack Manager",
-       options = {"build_exe": {}},
-       executables = [Executable("bbs.py")]
-)
