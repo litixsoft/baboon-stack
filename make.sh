2fe3e8fbd4c1bea4b74e5a234c5b34031e554656
Thomas Scheibe <th.scheibe@litixsoft.de>
2013-12-16 09:31:57 +0100

    Unix build script (permissions)

--- make.sh
+++ make.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+if [ -d "build" ]; then
+ rm -R build
+fi
+
+echo "Build..."
 python3 build.py bbs.py --target-dir "build" --icon "ressources/baboonstack.ico" --compress
+
+echo "Change permissions..."
+chmod 644 build/*
+chmod +x build/bbs
+
+echo "Done..."
